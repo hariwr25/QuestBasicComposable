360a88e5084549cbc771a551f0f64ad880823fcf
hariwr <hariwr25@gmail.com>
2024-10-17 22:20:36 +0700

    Add Color Font Text Login

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,6 +41,7 @@ Column(
         style = TextStyle(
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
+            color = Color.Black
         )
     )
 }
