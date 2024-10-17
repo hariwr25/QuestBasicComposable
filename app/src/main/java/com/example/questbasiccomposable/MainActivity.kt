fdbc8fd7121674296039ec78b1f887792fb8c5a5
hariwr <hariwr25@gmail.com>
2024-10-17 22:16:28 +0700

    Add TextStyle

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 
@@ -34,6 +35,9 @@ Column(
 ) {
     Text(
         text = "Login",
+        style = TextStyle(
+
+        )
     )
 }
 }
