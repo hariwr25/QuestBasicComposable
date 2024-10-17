73288f4ea825c8a09ca572669563ecbc6622d405
hariwr <hariwr25@gmail.com>
2024-10-17 23:19:34 +0700

    Memanggil dan edit size profil image

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -99,6 +101,14 @@ Column(
         )
     )
 
+    Image(
+        painter = painterResource(id = R.drawable.foto),
+        contentDescription = "Profil",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(300.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
 }
 }
 
