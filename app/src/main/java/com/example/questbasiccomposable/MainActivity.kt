c707fbc4c34a1edf2fbedcb72823feac5e4b4d8a
hariwr <hariwr25@gmail.com>
2024-10-17 22:17:27 +0700

    Add Fontsize

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +37,7 @@ Column(
     Text(
         text = "Login",
         style = TextStyle(
-
+            fontSize = 32.sp,
         )
     )
 }
